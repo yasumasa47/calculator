22606d1781679528938e11b60670fd0942a78d46
yasumasa <yasumasa47.dev@gmail.com>
2023-12-14 00:10:21 +0900

    2023/12/14-00:10

---
+++ lib/routers/my_app.dart
@@ -0,0 +1,13 @@
+import 'package:calculator/pages/calculator_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CalculatorPage(),
+    );
+  }
+}
